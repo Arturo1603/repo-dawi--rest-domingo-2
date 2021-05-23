8755198ba49bcbbf20233946470483f3e0477dc9
Arturo1603 <arturo.cq.16@gmail.com>
2021-05-23 12:07:13 -0500

    busca

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -7,6 +7,7 @@ import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -63,4 +64,15 @@ public class AlumnoController {
 			return ResponseEntity.badRequest().build();
 		}
 	} 
-}
+	
+	@GetMapping("/{dni}")
+	public ResponseEntity<List<Alumno>> buscapordni(@PathVariable("dni") String dni){
+		System.out.println(">>> inicio >>> lista " );
+		List<Alumno> optAlumno = service.listaPorDni(dni);
+		
+			service.listaPorDni(dni);
+			return ResponseEntity.ok(optAlumno);
+		
+		}
+	} 
+
